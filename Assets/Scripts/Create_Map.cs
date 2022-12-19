4757e0beabb3a19ca6590ced483ae15d783dd977
Igor <roggi.7@mail.ru>
2022-12-20 04:25:22 +0500

    final_generate

--- Assets/Scripts/Create_Map.cs
+++ Assets/Scripts/Create_Map.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Create_Map : MonoBehaviour
 {
+    public GameObject car;
     public GameObject z1;
     public GameObject z2;
     public GameObject z3;
@@ -14,155 +15,351 @@ public class Create_Map : MonoBehaviour
     public GameObject z8;
     public GameObject z9;
     public GameObject z10;
+    public GameObject cont;
     public bool first = true;
-    private int wall = 1;
-    void Start()
+    private readonly int size = 4;
+    public float CT = 43.5f;
+    public int counter = 0;
+    public float min_x = 0f;
+    readonly Queue<GameObject> road = new Queue<GameObject>();
+    //void Start()
+    //{
+    //    Queue<GameObject> road = new Queue<GameObject>();
+    //    road.Enqueue(z1);
+    //    int exit = 0;
+    //    Generate(z1, exit, road);
+    //}
+
+    //private void Update(Queue<GameObject> road)
+    //{
+    //   road.Dequeue
+    //}
+
+    //public void Generate(GameObject q, int ex, Queue<GameObject> road)
+    //{
+    //    GameObject cell;
+    //    float x = q.transform.localPosition.x;
+    //    float y = q.transform.localPosition.y;
+    //    int rand = Random.Range(2, 11);
+    //    if (rand == 2 && ex <= wall)
+    //    {
+    //        cell = Instantiate(z2);
+    //        if (first)
+    //        {
+    //            cell.transform.localPosition = new Vector3(x + 10, y, 0);
+    //            first = false;
+    //        }
+    //        else
+    //        {
+    //            cell.transform.localPosition = new Vector3(x + 10, y, 0);
+    //        }
+    //        ex++;
+    //        road.Enqueue(cell);
+    //        if (road.Count == wall - 5)
+    //        {
+    //            Debug.Log(road.Count);
+    //            Destroy(road.Peek());
+    //            Debug.Log(road.Count);
+    //            Debug.Log(road.Peek());
+    //            road.Dequeue();
+    //            Debug.Log(road.Count);
+    //            Debug.Log(road.Peek());
+    //        }
+    //        Generate(cell, ex, road);
+
+    //    }
+    //    else if (rand == 3 && ex <= wall)
+    //    {
+    //        cell = Instantiate(z3);
+    //        if (first)
+    //        {
+    //            cell.transform.localPosition = new Vector3(x + 10, y, 0);
+    //            first = false;
+    //        }
+    //        else
+    //        {
+    //            cell.transform.localPosition = new Vector3(x + 10, y, 0);
+    //        }
+    //        ex++;
+    //        road.Enqueue(cell);
+    //        if (road.Count == wall - 5)
+    //        {
+    //            Debug.Log(road.Count);
+    //            Destroy(road.Peek());
+    //            Debug.Log(road.Count);
+    //            Debug.Log(road.Peek());
+    //            road.Dequeue();
+    //            Debug.Log(road.Count);
+    //            Debug.Log(road.Peek());
+    //        }
+    //        Generate(cell, ex, road);
+    //    }
+    //    else if (rand == 4 && ex <= wall)
+    //    {
+    //        cell = Instantiate(z4);
+    //        if (first)
+    //        {
+    //            cell.transform.localPosition = new Vector3(x + 10, y, 0);
+    //            first = false;
+    //        }
+    //        else
+    //        {
+    //            cell.transform.localPosition = new Vector3(x + 10, y, 0);
+    //        }
+    //        ex++;
+    //        road.Enqueue(cell);
+    //        if (road.Count == wall - 5)
+    //        {
+    //            Debug.Log(road.Count);
+    //            Destroy(road.Peek());
+    //            Debug.Log(road.Count);
+    //            Debug.Log(road.Peek());
+    //            road.Dequeue();
+    //            Debug.Log(road.Count);
+    //            Debug.Log(road.Peek());
+    //        }
+    //        Generate(cell, ex, road);
+    //    }
+    //    else if (rand == 5 && ex <= wall)
+    //    {
+    //        cell = Instantiate(z5);
+    //        if (first)
+    //        {
+    //            cell.transform.localPosition = new Vector3(x + 10, y, 0);
+    //            first = false;
+    //        }
+    //        else
+    //        {
+    //            cell.transform.localPosition = new Vector3(x + 10, y, 0);
+    //        }
+    //        ex++;
+    //        road.Enqueue(cell);
+    //        if (road.Count == wall - 5)
+    //        {
+    //            Debug.Log(road.Count);
+    //            Destroy(road.Peek());
+    //            Debug.Log(road.Count);
+    //            Debug.Log(road.Peek());
+    //            road.Dequeue();
+    //            Debug.Log(road.Count);
+    //            Debug.Log(road.Peek());
+
+    //        }
+    //        Generate(cell, ex, road);
+    //    }
+    //    else if (rand == 6 && ex <= wall)
+    //    {
+    //        cell = Instantiate(z6);
+    //        if (first)
+    //        {
+    //            cell.transform.localPosition = new Vector3(x + 10, y, 0);
+    //            first = false;
+    //        }
+    //        else
+    //        {
+    //            cell.transform.localPosition = new Vector3(x + 10, y, 0);
+    //        }
+    //        ex++;
+    //        road.Enqueue(cell);
+    //        if (road.Count == wall - 5)
+    //        {
+    //            Debug.Log(road.Count);
+    //            Destroy(road.Peek());
+    //            Debug.Log(road.Count);
+    //            Debug.Log(road.Peek());
+    //            road.Dequeue();
+    //            Debug.Log(road.Count);
+    //            Debug.Log(road.Peek());
+    //        }
+    //        Generate(cell, ex, road);
+    //    }
+    //    else if (rand == 7 && ex <= wall)
+    //    {
+    //        cell = Instantiate(z7);
+    //        if (first)
+    //        {
+    //            cell.transform.localPosition = new Vector3(x + 10, y, 0);
+    //            first = false;
+    //        }
+    //        else
+    //        {
+    //            cell.transform.localPosition = new Vector3(x + 10, y, 0);
+    //        }
+    //        ex++;
+    //        road.Enqueue(cell);
+    //        if (road.Count == wall - 5)
+    //        {
+    //            Debug.Log(road.Count);
+    //            Destroy(road.Peek());
+    //            Debug.Log(road.Count);
+    //            Debug.Log(road.Peek());
+    //            road.Dequeue();
+    //            Debug.Log(road.Count);
+    //            Debug.Log(road.Peek());
+    //        }
+    //        Generate(cell, ex, road);
+    //    }
+    //    else if (rand == 8 && ex <= wall)
+    //    {
+    //        cell = Instantiate(z8);
+    //        if (first)
+    //        {
+    //            cell.transform.localPosition = new Vector3(x + 10, y, 0);
+    //            first = false;
+    //        }
+    //        else
+    //        {
+    //            cell.transform.localPosition = new Vector3(x + 10, y, 0);
+    //        }
+    //        road.Enqueue(cell);
+    //        if (road.Count == wall - 5)
+    //        {
+    //            Debug.Log(road.Count);
+    //            Destroy(road.Peek());
+    //            Debug.Log(road.Count);
+    //            Debug.Log(road.Peek());
+    //            road.Dequeue();
+    //            Debug.Log(road.Count);
+    //            Debug.Log(road.Peek());
+    //        }
+    //        ex++;
+    //        Generate(cell, ex, road);
+    //    }
+    //    else if (rand == 9 && ex <= wall)
+    //    {
+    //        cell = Instantiate(z9);
+    //        if (first)
+    //        {
+    //            cell.transform.localPosition = new Vector3(x + 10, y, 0);
+    //            first = false;
+    //        }
+    //        else
+    //        {
+    //            cell.transform.localPosition = new Vector3(x + 10, y, 0);
+    //        }
+    //        ex++;
+    //        road.Enqueue(cell);
+    //        if (road.Count == wall - 5)
+    //        {
+    //            Debug.Log(road.Count);
+    //            Destroy(road.Peek());
+    //            Debug.Log(road.Count);
+    //            Debug.Log(road.Peek());
+    //            road.Dequeue();
+    //            Debug.Log(road.Count);
+    //            Debug.Log(road.Peek());
+    //        }
+    //        Generate(cell, ex, road);
+    //    }
+    //    else if (rand == 10 && ex <= wall)
+    //    {
+    //        cell = Instantiate(z10);
+    //        if (first)
+    //        {
+    //            cell.transform.localPosition = new Vector3(x + 10, y, 0);
+    //            first = false;
+    //        }
+    //        else
+    //        {
+    //            cell.transform.localPosition = new Vector3(x + 10, y, 0);
+    //        }
+    //        ex++;
+    //        road.Enqueue(cell);
+    //        if (road.Count == wall - 5)
+    //        {
+    //            Debug.Log(road.Count);
+    //            Destroy(road.Peek());
+    //            Debug.Log(road.Count);
+    //            Debug.Log(road.Peek());
+    //            road.Dequeue();
+    //            Debug.Log(road.Count);
+    //            Debug.Log(road.Peek());
+    //        }
+    //        Generate(cell, ex, road);
+    //    }
+    //    if ( ex > wall)
+    //    {
+
+    //    }
+    //}
+
+    private void Start()
     {
-        int exit = 0;
-        Generate(z1, exit);
+        CreateBase();
     }
 
-    public void Generate(GameObject q, int ex)
+    private void Update()
     {
+        if (CT - car.transform.localPosition.x <= 15)
+        {
+            Generate();
+            if (first)
+            {
+                Destroy(road.Peek());
 
-        GameObject cell;
-        float x = q.transform.localPosition.x;
-        float y = q.transform.localPosition.y;
-        int rand = Random.Range(2, 11);
-        if (rand == 2 && ex <= wall)
-        {
-            cell = Instantiate(z2, q.transform);
-            if (first)
-            {
-                cell.transform.localPosition = new Vector3(x + 10, y, 0);
-                first = false;
+                road.Dequeue();
             }
             else
-            {
-                cell.transform.localPosition = new Vector3(x, y, 0);
-            }
-            ex++;
-            Generate(cell, ex);
+                first = true;
+
+            CT += 10;
+            min_x += 10;
         }
-        else if (rand == 3 && ex <= wall)
+        if (min_x > car.transform.localPosition.x)
         {
-            cell = Instantiate(z3, q.transform);
-            if (first)
-            {
-                cell.transform.localPosition = new Vector3(x + 10, y, 0);
-                first = false;
-            }
-            else
-            {
-                cell.transform.localPosition = new Vector3(x, y, 0);
-            }
-            ex++;
-            Generate(cell, ex);
+            car.transform.localPosition = new Vector3(min_x, car.transform.localPosition.y, car.transform.localPosition.z);
         }
-        else if (rand == 4 && ex <= wall)
+    }
+
+    public void Generate()
+    {
+        GameObject cell = cont;
+        float x = cell.transform.localPosition.x;
+        float y = cell.transform.localPosition.y;
+        int rand = Random.Range(1, 11);
+        if (rand == 1)
+            cell = Instantiate(z1);
+        else if (rand == 2)
+            cell = Instantiate(z2);
+        else if (rand == 3)
+            cell = Instantiate(z3);
+        else if (rand == 4)
+            cell = Instantiate(z4);
+        else if (rand == 5)
+            cell = Instantiate(z5);
+        else if (rand == 6)
+            cell = Instantiate(z6);
+        else if (rand == 7)
+            cell = Instantiate(z7);
+        else if (rand == 8)
+            cell = Instantiate(z8);
+        else if (rand == 9)
+            cell = Instantiate(z9);
+        else if (rand == 10)
+            cell = Instantiate(z10);
+
+        cell.transform.localPosition = new Vector3(x + 10, y, 0);
+        road.Enqueue(cell);
+        cont = cell;
+    }
+
+    public void CreateBase()
+    {
+        GameObject cell = Instantiate(z1);
+        if (first)
         {
-            cell = Instantiate(z4, q.transform);
-            if (first)
-            {
-                cell.transform.localPosition = new Vector3(x + 10, y, 0);
-                first = false;
-            }
-            else
-            {
-                cell.transform.localPosition = new Vector3(x, y, 0);
-            }
-            ex++;
-            Generate(cell, ex);
+            cell.transform.localPosition = new Vector3(0, 0, 0);
+            first = false;
+            counter++;
         }
-        else if (rand == 5 && ex <= wall)
+        else
         {
-            cell = Instantiate(z5, q.transform);
-            if (first)
-            {
-                cell.transform.localPosition = new Vector3(x + 10, y, 0);
-                first = false;
-            }
-            else
-            {
-                cell.transform.localPosition = new Vector3(x, y, 0);
-            }
-            ex++;
-            Generate(cell, ex);
+            cell.transform.localPosition = new Vector3(cont.transform.localPosition.x + 10, 0, 0);
+            counter++;
         }
-        else if (rand == 6 && ex <= wall)
-        {
-            cell = Instantiate(z6, q.transform);
-            if (first)
-            {
-                cell.transform.localPosition = new Vector3(x + 10, y, 0);
-                first = false;
-            }
-            else
-            {
-                cell.transform.localPosition = new Vector3(x, y, 0);
-            }
-            ex++;
-            Generate(cell, ex);
-        }
-        else if (rand == 7 && ex <= wall)
-        {
-            cell = Instantiate(z7, q.transform);
-            if (first)
-            {
-                cell.transform.localPosition = new Vector3(x + 10, y, 0);
-                first = false;
-            }
-            else
-            {
-                cell.transform.localPosition = new Vector3(x, y, 0);
-            }
-            ex++;
-            Generate(cell, ex);
-        }
-        else if (rand == 8 && ex <= wall)
-        {
-            cell = Instantiate(z8, q.transform);
-            if (first)
-            {
-                cell.transform.localPosition = new Vector3(x + 10, y, 0);
-                first = false;
-            }
-            else
-            {
-                cell.transform.localPosition = new Vector3(x, y, 0);
-            }
-            ex++;
-            Generate(cell, ex);
-        }
-        else if (rand == 9 && ex <= wall)
-        {
-            cell = Instantiate(z9, q.transform);
-            if (first)
-            {
-                cell.transform.localPosition = new Vector3(x + 10, y, 0);
-                first = false;
-            }
-            else
-            {
-                cell.transform.localPosition = new Vector3(x, y, 0);
-            }
-            ex++;
-            Generate(cell, ex);
-        }
-        else if (rand == 10 && ex <= wall)
-        {
-            cell = Instantiate(z10, q.transform);
-            if (first)
-            {
-                cell.transform.localPosition = new Vector3(x + 10, y, 0);
-                first = false;
-            }
-            else
-            {
-                cell.transform.localPosition = new Vector3(x, y, 0);
-            }
-            ex++;
-            Generate(cell, ex);
-        }
+        road.Enqueue(cell);
+        cont = cell;
+        if (counter < size)
+            CreateBase();
     }
 }
